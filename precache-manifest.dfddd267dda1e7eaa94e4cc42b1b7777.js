5cc7f078289f43d4c9e68dc8e427af65dae36dd1
mtisel <maja.tisel@gmail.com>
2020-05-21 14:39:43 +0200

    Updates

---
+++ precache-manifest.dfddd267dda1e7eaa94e4cc42b1b7777.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1184f2aef7f1c6346f7e34b6ddc9ef0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36dbeaa91e2e7c7a3397",
+    "url": "/static/css/main.e351a5e7.chunk.css"
+  },
+  {
+    "revision": "af34272c1f58063b1a97",
+    "url": "/static/js/2.bd5b9316.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd5b9316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36dbeaa91e2e7c7a3397",
+    "url": "/static/js/main.a81bf78c.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "f25369761f5766621f4cbbb282820253",
+    "url": "/static/media/Balanced1.f2536976.gif"
+  },
+  {
+    "revision": "635aa5391de612a24a29c5fc128f0836",
+    "url": "/static/media/Balanced2.635aa539.jpg"
+  },
+  {
+    "revision": "14a45d9d9288b0ee4cc7e5fe1f0ceec7",
+    "url": "/static/media/Balanced3.14a45d9d.jpg"
+  },
+  {
+    "revision": "4a24899e94d8236f671c1090cd9e068c",
+    "url": "/static/media/Canela-Regular.4a24899e.ttf"
+  },
+  {
+    "revision": "4dc172158023f4f3f1776f365a0f29d5",
+    "url": "/static/media/Daylight1.4dc17215.jpg"
+  },
+  {
+    "revision": "b72e282af0922a951a138457e34b9177",
+    "url": "/static/media/Daylight2.b72e282a.gif"
+  },
+  {
+    "revision": "512a202711c9ee344df87e84c12d93e4",
+    "url": "/static/media/Mag1.512a2027.jpg"
+  },
+  {
+    "revision": "f419ed4fb9e63aa94d058d858e401cf7",
+    "url": "/static/media/Mag2.f419ed4f.png"
+  },
+  {
+    "revision": "a36c3c742639aca2fe5aafec520d85af",
+    "url": "/static/media/Mag3.a36c3c74.jpg"
+  },
+  {
+    "revision": "24cc028411d3bb3e018acc6cdf41146b",
+    "url": "/static/media/Memberships1.24cc0284.jpg"
+  },
+  {
+    "revision": "97d272cdbde60567967cef7593cfb393",
+    "url": "/static/media/Memberships2.97d272cd.jpg"
+  },
+  {
+    "revision": "a0a8529e9202de94037c77574515a30f",
+    "url": "/static/media/Memberships3.a0a8529e.png"
+  },
+  {
+    "revision": "330da57490cf41f49731af5152ba6a02",
+    "url": "/static/media/Pride1.330da574.png"
+  },
+  {
+    "revision": "f6c12d92045dcb3e6f07f6e14c6932b3",
+    "url": "/static/media/Pride2.f6c12d92.png"
+  },
+  {
+    "revision": "cd2a351fa1f984aafcef956d219c16b9",
+    "url": "/static/media/Pride3.cd2a351f.png"
+  },
+  {
+    "revision": "9ffa3cd5fe2dbf09bcacd3069508d0dc",
+    "url": "/static/media/Reason1.9ffa3cd5.jpg"
+  },
+  {
+    "revision": "521f9b9a58c3aff3faf735d20cc8c7eb",
+    "url": "/static/media/Reason2.521f9b9a.jpg"
+  },
+  {
+    "revision": "9b56bd7ca162c6e60b051de4fcc7237b",
+    "url": "/static/media/Reason3.9b56bd7c.gif"
+  },
+  {
+    "revision": "c62886d16690b9e5676a47da5384b2aa",
+    "url": "/static/media/Rebrand1.c62886d1.gif"
+  },
+  {
+    "revision": "43c1f04c1499ac95b427dca357a0a8a7",
+    "url": "/static/media/Tiger1.43c1f04c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mtisel.github.io/precache-manifest.8c269d61dde7d08f6642a58ad8ace325.js"
+  "/precache-manifest.dfddd267dda1e7eaa94e4cc42b1b7777.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mtisel.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
